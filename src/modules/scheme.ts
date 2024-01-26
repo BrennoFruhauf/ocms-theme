a41e87d2c336ec9afd097685ec383614cfb49a6a
Brenno <brennofruhauf@outlook.com>
2024-01-25 22:03:46 -0300

    feat: add contrast color check

--- src/modules/scheme.ts
+++ src/modules/scheme.ts
@@ -14,7 +14,7 @@ const scheme = (colors: [string[], string[]], override: Scheme | null = null) =>
 
 	if (override != null) merge(draft, override)
 
-	transform(draft, (value: Color) => colorize(colors, value))
+	transform(draft, (key: string, value: Color) => colorize(colors, key, value))
 
 	return draft;
 }
@@ -26,7 +26,35 @@ const scheme = (colors: [string[], string[]], override: Scheme | null = null) =>
  * @param value Key value with color position and alpha.
  * @returns RRGGBBAA | RRGGBB color in the hex format.
  */
-const colorize = (colors: [string[], string[]], value: Color) => chroma(colors[value.array][value.position]).alpha(value.alpha ?? 1).hex()
+const colorize = (colors: [string[], string[]], key: string, value: Color) => {
+	const color: string = keyToCheck.some(k => k === key) ? checkContrast(colors, value) : colors[value.array][value.position]
+	return chroma(color).alpha(value.alpha ?? 1).hex()
+}
+
+/**
+ * Array containing predefined keys related to check color contrast.
+ *
+ * @type `String[]`
+ */
+const keyToCheck: string[] = [
+	'activityBarBadge.foreground',
+	'statusBar.foreground',
+	'menu.selectionForeground',
+	'button.foreground',
+	'inputOption.activeForeground',
+	'badge.foreground',
+	'terminalCursor.foreground',
+	'notificationCenterHeader.foreground'
+]
+
+/**
+ * Checks the contrast between two colors based on specific conditions.
+ *
+ * @param c A tuple containing two color arrays.
+ * @param v Color information.
+ * @returns `String` with color suitable contrast based on the conditions.
+ */
+const checkContrast = (c: [string[], string[]], v: Color): string => chroma.contrast(c[v.array][v.position], c[0][0]) < 2 ? c[1][0] : c[v.array][v.position]
 
 /**
  * Deep object transformer.
@@ -44,7 +72,7 @@ const transform = (object: Record<string, any>, transformation: Function) => {
 		}
 
 		if (typeof object[key] !== 'object') continue
-		object[key] = transformation.apply(this, [object[key]]) || object[key]
+		object[key] = transformation.apply(this, [key, object[key]]) || object[key]
 	}
 	return object
 }
